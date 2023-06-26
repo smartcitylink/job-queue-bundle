19c5d592aacf8b9b3633e4f5cd3e885daa7147ee
Gustavo Hernáez <ghernaez@smartcity.link>
2023-06-26 14:01:24 +0200

    The following deprecations are solved before upgrading to Symfony 5:
    - User Deprecated: The "JMS\JobQueueBundle\Controller\JobController" class extends "Symfony\Bundle\FrameworkBundle\Controller\Controller" that is deprecated since Symfony 4.2, use "Symfony\Bundle\FrameworkBundle\Controller\AbstractController" instead.
    - User Deprecated: The "Sensio\Bundle\FrameworkExtraBundle\Configuration\Route" annotation is deprecated since version 5.2. Use "Symfony\Component\Routing\Annotation\Route" instead.
    - A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0.
    - The "Symfony\Component\Config\Definition\Builder\TreeBuilder::root()" method called for the "jms_job_queue" configuration is deprecated since Symfony 4.3, pass the root name to the constructor instead.
    - The doctrine-bundle type commenting features were removed; the corresponding config parameter was deprecated in 2.0 and will be dropped in 3.0.

--- Entity/Type/SafeObjectType.php
+++ Entity/Type/SafeObjectType.php
@@ -3,9 +3,15 @@
 namespace JMS\JobQueueBundle\Entity\Type;
 
 use Doctrine\DBAL\Types\ObjectType;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 
 class SafeObjectType extends ObjectType
 {
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return true;
+    }
+
     public function getSQLDeclaration(array $fieldDeclaration, \Doctrine\DBAL\Platforms\AbstractPlatform $platform)
     {
         return $platform->getBlobTypeDeclarationSQL($fieldDeclaration);
